8c0f63117195abc054c9b599cb79724698e21513
Mariana Garcia <maridgarcia045@gmail.com>
2024-02-19 19:11:40 -0300

    fix: classNames

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -23,7 +23,7 @@ function Location() {
           
           </div>
           <div className="order-1 lg:order-2">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d916.9388018351592!2d-48.58504845869388!3d-25.933359329737268!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94dbfb13f62b2cf3%3A0x126ce3bb6ffc556c!2sAv.%20Visc.%20do%20Rio%20Branco%2C%205000%20-%20Nereidas%2C%20Guaratuba%20-%20PR%2C%2083280-000!5e0!3m2!1spt-BR!2sbr!4v1707262483537!5m2!1spt-BR!2sbr" width="600" height="450" style={{border:0}}allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d916.9388018351592!2d-48.58504845869388!3d-25.933359329737268!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94dbfb13f62b2cf3%3A0x126ce3bb6ffc556c!2sAv.%20Visc.%20do%20Rio%20Branco%2C%205000%20-%20Nereidas%2C%20Guaratuba%20-%20PR%2C%2083280-000!5e0!3m2!1spt-BR!2sbr!4v1707262483537!5m2!1spt-BR!2sbr" width="600" height="450" style={{border:0}}allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
           </div>
         </div>
       </section>
